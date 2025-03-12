ea661f42931b9a24a83e8b15211060118b0d1d23
AndresM2003 <andrescamilom2003@gmail.com>
2025-03-11 19:50:57 -0500

    primer commit

---
+++ main.py
@@ -0,0 +1,11 @@
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def saludar(self):
+        print(f"Hola, mi nombre es {self.nombre} y tengo {self.edad} años.")
+
+# Crear una instancia de la clase Persona
+persona1 = Persona("Andres", 25)
+persona1.saludar()
